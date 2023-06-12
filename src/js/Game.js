3486c905d3c5df3795b9941d755732133c1c6f68
uri752 <uri752blag@mail.ru>
2023-06-12 20:18:58 +0300

    init commit

---
+++ src/js/Game.js
@@ -0,0 +1,35 @@
+export default class Game {
+    constructor() {
+        this.board = document.getElementById("board");        
+        const goblin = document.getElementById('goblin')  
+        goblin.className='';      
+        this.goblin = goblin;
+        this.curTileIndex = -1;
+        this.generateMatrix();                
+    }
+
+    generateMatrix() {        
+
+        const newIndex = Math.floor(Math.random() * 16);            
+        this.matrix = [
+            undefined, undefined, undefined, undefined, 
+            undefined, undefined, undefined, undefined, 
+            undefined, undefined, undefined, undefined, 
+            undefined, undefined, undefined, undefined, 
+
+        ];
+
+        this.matrix.forEach( (item, index, arr) => {            
+            const tile = document.createElement('span');
+            tile.className = 'tile';    
+            this.board.appendChild(tile);            
+            if (index === newIndex) {
+                tile.appendChild(this.goblin);
+            }            
+            //item = tile;
+            arr[index] = tile;
+        });
+
+        this.curTileIndex = newIndex;                
+    }
+}
---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+import Game from './Game';
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const game = new Game();
+
+    const interval = setInterval( () => {
+                
+        const index = Math.floor(Math.random() * 16);                   
+        game.curTileIndex = index;                
+        game.matrix[game.curTileIndex].appendChild(game.goblin);
+
+    }, 1000);
+
+});
